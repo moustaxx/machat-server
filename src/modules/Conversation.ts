1cb07681d043b931cd4525c91947dc44f6a84dcd
moustaxx <maciasm1303@gmail.com>
2020-08-13 21:53:47 +0200

    Add mutations: remove / add Person to Conversation

--- src/modules/Conversation.ts
+++ src/modules/Conversation.ts
@@ -1,4 +1,5 @@
-import { objectType } from '@nexus/schema';
+import { objectType, mutationField, intArg } from '@nexus/schema';
+import { ForbiddenError, ApolloError } from 'apollo-server-errors';
 
 // eslint-disable-next-line import/prefer-default-export
 export const Conversation = objectType({
@@ -11,3 +12,57 @@ export const Conversation = objectType({
         t.model.participants();
     },
 });
+
+export const addPersonToConversationMutationField = mutationField('addPersonToConversation', {
+    type: 'Conversation',
+    args: {
+        personId: intArg({ required: true }),
+        conversationId: intArg({ required: true }),
+    },
+    resolve: async (_root, args, { prisma, session }) => {
+        if (!session || !session.isLoggedIn || !session.owner) {
+            throw new ForbiddenError('You must be logged in!');
+        }
+
+        const data = await prisma.conversation.update({
+            where: { id: args.conversationId },
+            data: {
+                participants: {
+                    connect: { id: args.personId },
+                },
+            },
+        });
+
+        if (!data) throw new ApolloError('No data!', 'NO_DATA');
+
+        return data;
+    },
+});
+
+export const removePersonFromConversationMutationField = mutationField(
+    'removePersonFromConversation', {
+        type: 'Conversation',
+        args: {
+            personId: intArg({ required: true }),
+            conversationId: intArg({ required: true }),
+        },
+        resolve: async (_root, args, { prisma, session }) => {
+            if (!session || !session.isLoggedIn || !session.owner) {
+                throw new ForbiddenError('You must be logged in!');
+            }
+
+            const data = await prisma.conversation.update({
+                where: { id: args.conversationId },
+                data: {
+                    participants: {
+                        disconnect: { id: args.personId },
+                    },
+                },
+            });
+
+            if (!data) throw new ApolloError('No data!', 'NO_DATA');
+
+            return data;
+        },
+    },
+);
