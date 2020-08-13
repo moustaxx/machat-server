aa3738e4520ec05b6a3662b763292ddd1d67c485
moustaxx <maciasm1303@gmail.com>
2020-08-13 22:04:44 +0200

    Add: createMessage mutation

--- src/modules/Message.ts
+++ src/modules/Message.ts
@@ -1,4 +1,5 @@
-import { objectType } from '@nexus/schema';
+import { objectType, mutationField, intArg, stringArg } from '@nexus/schema';
+import { ForbiddenError, ApolloError } from 'apollo-server-errors';
 
 // eslint-disable-next-line import/prefer-default-export
 export const Message = objectType({
@@ -11,5 +12,34 @@ export const Message = objectType({
         t.model.authorID();
         t.model.author();
         t.model.createdAt();
+    },
+});
+
+export const createMessageMutationField = mutationField('createMessage', {
+    type: 'Message',
+    args: {
+        content: stringArg({ required: true }),
+        conversationId: intArg({ required: true }),
+    },
+    resolve: async (_root, args, { prisma, session }) => {
+        if (!session || !session.isLoggedIn || !session.owner) {
+            throw new ForbiddenError('You must be logged in!');
+        }
+
+        const data = await prisma.message.create({
+            data: {
+                content: args.content,
+                author: {
+                    connect: { id: session.owner.id },
+                },
+                conversation: {
+                    connect: { id: args.conversationId },
+                },
+            },
+        });
+
+        if (!data) throw new ApolloError('No data!', 'NO_DATA');
+
+        return data;
     },
 });
--- src/modules/Mutation.ts
+++ src/modules/Mutation.ts
@@ -4,7 +4,6 @@ import { objectType } from '@nexus/schema';
 export const Mutation = objectType({
     name: 'Mutation',
     definition(t) {
-        t.crud.createOneMessage();
         t.crud.deleteOneMessage();
         t.crud.updateOneMessage();
 
