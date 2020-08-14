fbd440e1b483fffff605f9689ad926efad724da3
moustaxx <maciasm1303@gmail.com>
2020-08-14 22:19:30 +0200

    Add isAdmin field to Person

--- src/modules/Person.ts
+++ src/modules/Person.ts
@@ -9,6 +9,7 @@ export const Person = objectType({
         t.model.username();
         t.model.email();
         t.model.isActive();
+        t.model.isAdmin();
         t.model.createdAt();
         t.model.lastSeen();
         t.model.messages();
