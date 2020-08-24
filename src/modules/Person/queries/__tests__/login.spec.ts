85fa0c82fc7a64903b17466eec6859a0278d13f3
moustaxx <maciasm1303@gmail.com>
2020-08-24 21:16:31 +0200

    Test: error when already logged in user try to log

--- src/modules/Person/queries/__tests__/login.spec.ts
+++ src/modules/Person/queries/__tests__/login.spec.ts
@@ -4,7 +4,7 @@ import { createFastifyGQLTestClient } from 'fastify-gql-integration-testing';
 
 import main from '../../../..';
 import { NexusGenRootTypes } from '../../../../generated/nexus';
-import { createRandomUser } from '../../../../tests/helpers';
+import { createRandomUser, gqlRequest } from '../../../../tests/helpers';
 
 let client: PrismaClient;
 let app: FastifyInstance;
@@ -33,11 +33,43 @@ it('should log in', async () => {
             }
         }
     `, {
-        variables: {
-            username,
-            password,
-        },
+        variables: { username, password },
     });
 
     expect(data.login.id).toEqual(user.id);
+});
+
+it('should throw error when already logged in', async () => {
+    const { username, password } = await createRandomUser(client);
+
+    const loginRes = await gqlRequest(app, {
+        query: `
+            query login($username: String!, $password: String!) {
+                login(username: $username, password: $password) {
+                    id
+                }
+            }
+        `,
+        variables: { username, password },
+    });
+
+    const dirtyCookies = loginRes.cookies;
+    let cookies: Record<string, string> = {};
+    dirtyCookies.forEach((cookie) => {
+        cookies = { ...cookies, [cookie.name]: cookie.value };
+    });
+
+    const { errors } = await testClient.query(`
+        query login($username: String!, $password: String!) {
+            login(username: $username, password: $password) {
+                id
+            }
+        }
+    `, {
+        cookies,
+        variables: { username, password },
+    });
+
+    const errorCode = errors && errors[0].extensions?.code;
+    expect(errorCode).toEqual('ALREADY_LOGGED_IN');
 });
