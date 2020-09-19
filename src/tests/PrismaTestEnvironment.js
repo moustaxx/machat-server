334f73eb535c65bb049736f178e62b34bdf0006c
moustaxx <maciasm1303@gmail.com>
2020-09-19 20:06:30 +0200

    Remove comments from PrismaTestEnvironment

--- src/tests/PrismaTestEnvironment.js
+++ src/tests/PrismaTestEnvironment.js
@@ -1,11 +1,8 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const { Client } = require('pg');
 const NodeEnvironment = require('jest-environment-node');
+const { Client } = require('pg');
 const util = require('util');
-const dotenv = require('dotenv');
 const path = require('path');
-
-dotenv.config();
 
 const exec = util.promisify(require('child_process').exec);
 
@@ -21,20 +18,15 @@ const execRepeatedOnErr = async (command, i = 0) => {
 class PrismaTestEnvironment extends NodeEnvironment {
     constructor(config) {
         super(config);
-        // Generate a unique schema identifier for this test context
-        this.schema = `test_${Date.now()}_${Math.random().toString(16).substring(2)}`;
 
-        // Generate the pg connection string for the test schema
+        this.schema = `test_${Date.now()}_${Math.random().toString(16).substring(2)}`;
         this.databaseUrl = `postgres://postgres:123456789@localhost:5432/postgres?schema=${this.schema}`;
     }
 
     async setup() {
-        // Set the required environment variable to contain the connection string
-        // to our database test schema
         process.env.DATABASE_URL = this.databaseUrl;
         this.global.process.env.DATABASE_URL = this.databaseUrl;
 
-        // Run the migrations to ensure our schema has the required structure
         await execRepeatedOnErr(`${prismaBinary} migrate save --experimental --name testing`);
         await execRepeatedOnErr(`${prismaBinary} migrate up --create-db --experimental`);
 
@@ -42,7 +34,6 @@ class PrismaTestEnvironment extends NodeEnvironment {
     }
 
     async teardown() {
-    // Drop the schema after the tests have completed
         const client = new Client({
             connectionString: this.databaseUrl,
         });
