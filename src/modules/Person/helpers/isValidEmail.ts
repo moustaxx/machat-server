c1b138d0e59f88027a1fe2b47723631ddcc4d340
moustaxx <maciasm1303@gmail.com>
2021-01-11 17:54:31 +0100

    IsValidEmail: replace String.match with Regex.test

--- src/modules/Person/helpers/isValidEmail.ts
+++ src/modules/Person/helpers/isValidEmail.ts
@@ -1,8 +1,8 @@
 const regexp = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
 const isValidEmail = (email: string): boolean => {
-    if (!email || email.length > 64 || !email.match(regexp)) return false;
-    return true;
+    if (!email || email.length > 64) return false;
+    return regexp.test(email);
 };
 
 export default isValidEmail;
