bf53749c4620416ff55f18b22eb9474ddbbb173c
moustaxx <maciasm1303@gmail.com>
2021-03-06 20:16:53 +0100

    Add LastRead realation resolvers

--- src/modules/LastRead/LastReadType.ts
+++ src/modules/LastRead/LastReadType.ts
@@ -1,5 +1,51 @@
-import { ObjectType } from 'type-graphql';
-import { LastRead } from '../../generated/type-graphql';
+import { Ctx, Field, FieldResolver, Int, ObjectType, Resolver, Root } from 'type-graphql';
+import { Context } from '../../context';
+import { ConversationType } from '../Conversation';
+import { PersonType } from '../Person';
 
 @ObjectType()
-export class LastReadType extends LastRead { }
+export class LastReadType {
+    @Field((_type) => Date)
+    lastRead!: Date;
+
+    @Field((_type) => Int)
+    personID!: number;
+
+    @Field((_type) => Int)
+    conversationID!: number;
+
+    person?: PersonType;
+
+    conversation?: ConversationType;
+}
+
+const whereIDEqalsID = (lastRead: LastReadType) => ({
+    where: {
+        personID_conversationID: {
+            personID: lastRead.personID,
+            conversationID: lastRead.conversationID,
+        },
+    },
+});
+
+@Resolver((_of) => LastReadType)
+export class LastReadTypeResolver {
+    @FieldResolver((_type) => PersonType)
+    async person(
+    // eslint-disable-next-line @typescript-eslint/indent
+        @Root() lastRead: LastReadType,
+        @Ctx() { prisma }: Context,
+    ) {
+        return prisma.lastRead.findUnique(whereIDEqalsID(lastRead)).person();
+    }
+
+    @FieldResolver((_type) => ConversationType)
+    async conversation(
+    // eslint-disable-next-line @typescript-eslint/indent
+        @Root() lastRead: LastReadType,
+        @Ctx() { prisma }: Context,
+    ) {
+        return prisma.lastRead.findUnique(whereIDEqalsID(lastRead)).conversation();
+    }
+}
+
