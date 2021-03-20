9a7be1e283709dfec7f8b987810d00aa2ad818f0
moustaxx <maciasm1303@gmail.com>
2021-03-20 17:49:16 +0100

    Add node query

--- src/relay/NodeInterface.ts
+++ src/relay/NodeInterface.ts
@@ -1,6 +1,16 @@
 import { Field, ID, InterfaceType } from 'type-graphql';
+import { ConversationType, MessageType, PersonType } from '../modules';
 
-@InterfaceType({ description: 'An object with a global ID.' })
+@InterfaceType({
+    isAbstract: true,
+    description: 'An object with a global ID.',
+    resolveType: (value) => {
+        if ('createdAt' in value && 'name' in value) return ConversationType;
+        if ('authorID' in value) return MessageType;
+        if ('username' in value) return PersonType;
+        throw new Error('Unknown type');
+    },
+})
 export abstract class Node {
     @Field(() => ID, {
         name: 'id',
--- src/relay/NodeResolver.ts
+++ src/relay/NodeResolver.ts
@@ -1,8 +1,11 @@
 /* eslint-disable @typescript-eslint/indent */
-import { FieldResolver, Info, Resolver, Root } from 'type-graphql';
+import { Arg, Ctx, FieldResolver, ID, Info, Query, Resolver, Root } from 'type-graphql';
 
 import { Node } from './NodeInterface';
 import { Connection } from './ConnectionType';
+import { Context } from '../context';
+import { PersonResolver, ConversationResolver } from '../modules';
+import throwErrorWhenUnauthorized from '../helpers/throwErrorWhenUnauthorized';
 
 export type TNodeModel<TModel> = Omit<TModel, 'id' | 'globalId'> & {
     id: `${string}:${string}`;
@@ -30,5 +33,52 @@ export class NodeResolver {
         @Info() { parentType: { name } }: { parentType: { name: string } },
     ): string {
         return toGlobalId(name, id);
+    }
+
+    private async fetcher(globalId: string, ctx: Context | Context<true>) {
+        throwErrorWhenUnauthorized(ctx.session);
+        const { typename, id } = fromGlobalId(globalId);
+
+        switch (typename) {
+            case 'ConversationType': {
+                return ConversationResolver.prototype.conversation(
+                    { whereId: id },
+                    ctx as Context<true>,
+                );
+            }
+            case 'PersonType': {
+                return PersonResolver.prototype.person({ id }, ctx as Context<true>);
+            }
+            case 'MessageType': {
+                throw new Error('Unallowed type');
+            }
+            default: {
+                throw new Error('Unknown type');
+            }
+        }
+    }
+
+    @Query(() => Node, {
+        nullable: true,
+        description: 'Fetches an object given its global ID.',
+    })
+    async node(
+        @Arg('id', () => ID, { description: 'The global ID of the object.' })
+        globalId: string,
+        @Ctx() ctx: Context,
+    ): ReturnType<NodeResolver['fetcher']> {
+        return this.fetcher(globalId, ctx);
+    }
+
+    @Query(() => [Node], {
+        nullable: 'items',
+        description: 'Fetches objects given their global IDs.',
+    })
+    nodes(
+        @Arg('ids', () => [ID], { description: 'The global IDs of the objects.' })
+        globalIds: Array<string>,
+        @Ctx() ctx: Context,
+    ): Array<ReturnType<NodeResolver['fetcher']>> {
+        return globalIds.map(async (id) => this.fetcher(id, ctx));
     }
 }
